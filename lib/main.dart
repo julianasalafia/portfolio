f702178235b45c0600c5211babe1e2e0cc2c4135
julianasalafia <juliana.salafia1@gmail.com>
2023-07-15 14:29:11 -0300

    add informations

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'constants.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +15,63 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Juliana Salafia'),
+              Text(
+                  'Olá :) Sou a Juliana, desenvolvedora android, moro na cidade de Santos em São Paulo e atuo com desenvolvimento frontend mobile utilizando a linguagem Kotlin.'),
+              Text('Contato'),
+              InkWell(
+                child: Text('email'),
+                onTap: () =>
+                    launchUrl(Uri.parse('mailto:juliana.salafia@gmail.com')),
+              ),
+              InkWell(
+                child: Text('linkedin'),
+                onTap: () => launchUrl(
+                    Uri.parse('https://www.linkedin.com/in/julianasalafia/')),
+              ),
+              InkWell(
+                child: Text('github'),
+                onTap: () =>
+                    launchUrl(Uri.parse('https://github.com/julianasalafia')),
+              ),
+              InkWell(
+                child: Text('behance'),
+                onTap: () =>
+                    launchUrl(Uri.parse('https://www.behance.net/jssalafia')),
+              ),
+              Text('Experiência'),
+              Text('Desenvolvedora Android @ Player’s Bank'),
+              Text(
+                  'Itaú Unibanco, São Paulo, Brazil – (Maio 2021 - Abril 2023)'),
+              Text('descrição'),
+              Text('Social Media / SEO Analyst'),
+              Text(
+                  'FD Comunicação - PR Games&Esports, Jundiaí, Brasil – (Agosto 2018 - Maio 2021'),
+              Text('descrição'),
+              Text('HARD SKILLS'),
+              Text('Kotlin'),
+              Text('descrição'),
+              Text('SOFT SKILLS'),
+              Text('fofa'),
+              Text('descrição'),
+              Text('Idiomas'),
+              Text('Inglês'),
+              Text('nível'),
+              Text('Português'),
+              Text('nível'),
+              Text('Educação'),
+              Text('Design de Games'),
+              Text('descrição'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
