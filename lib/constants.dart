7d398f6dc6e35aada81ec3cdc95c394d3dca11b6
julianasalafia <juliana.salafia1@gmail.com>
2023-07-15 14:02:56 -0300

    TRY AGAIN :D

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
 
-const kTextColor = Color(0xFF149414);
-const kText2Color = Color(0x740B4D0B);
-const fontSize = 18.0;
-
-const kColorizeTextStyle = TextStyle(
-    fontSize: fontSize,
-    fontFamily: 'Courier',
-    color: kTextColor,
-    shadows: [
-      Shadow(
-        blurRadius: 5.0,
-        color: kTextColor,
-        offset: Offset(0, 0),
-      ),
-    ]);
